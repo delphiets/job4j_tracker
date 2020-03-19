2cc9c7a0a5f28ce9c9b4f6b76b4c33513894b08a
delphiets <delfiyets@gmail.com>
2020-03-19 15:14:35 +0200

    2. Перегрузка конструктора.[#245351]

--- src/main/java/ru/job4j/tracker/Animal.java
+++ src/main/java/ru/job4j/tracker/Animal.java
@@ -2,6 +2,12 @@ package ru.job4j.tracker;
 
 public class Animal {
 
+    private String name;
+
     public Animal() {
+    }
+
+    public Animal(String name) {
+        this.name = name;
     }
 }
