7a09a6326d4b4ee73b774ad83b07b17337961607
delphiets <delfiyets@gmail.com>
2020-04-01 14:31:33 +0300

    2. Scanner и чтение числа из консоли.[#245308]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -8,7 +8,8 @@ public class Matches {
 
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     private static int sum = 11;
-    private static boolean step  = true;
+    private static int count = 0;
+    private static String act;
 
     public static int gamer(int count) throws IOException {
         boolean game = true;
@@ -28,41 +29,31 @@ public class Matches {
         System.out.println("Игра 11 спичек.");
         System.out.println("\nПравила игры. \nНа столе лежат 11 спичек. \nДва игрока по очереди берут от 1 до 3 спичек. \nВыигрывает тот, кто забрал последние спички.\n");
 
-        boolean game = true;
+        boolean menu = true;
 
-        while (game) {
+        while (menu) {
             System.out.println("1. Начать игру");
             System.out.println("2. Выйти");
             int number = Integer.parseInt(reader.readLine());
 
             if (number == 1) {
-                while (game) {
+                while (true) {
                     if (sum > 0) {
-                        if (step) {
-                            System.out.println("Ход первого игрока. \nОсталось: " + sum + " из 11 спичек");
-                            int count = Integer.parseInt(reader.readLine());
-                            Matches.gamer(count);
-                            step = false;
-                        } else {
-                            System.out.println("Ход второго игрока.\nОсталось: " + sum + " из 11 спичек");
-                            int count = Integer.parseInt(reader.readLine());
-                            Matches.gamer(count);
-                            step = true;
-                        }
-                    } else if (step) {
-                        System.out.println("Выиграл второй игрок");
-                        game = false;
-                        break;
+                        act = count % 2 == 0 ? "первый" : "второй";
+                        count++;
+                        System.out.println("Ходит " + act + " игрок. \nОсталось: " + sum + " из 11 спичек");
+                        int tell = Integer.parseInt(reader.readLine());
+                        Matches.gamer(tell);
                     } else {
-                        System.out.println("Выиграл первый игрок");
-                        game = false;
+                        System.out.println("Выиграл " + act + " игрок\n");
+                        sum = 11;
+                        count = 0;
                         break;
                     }
                 }
-
             } else if (number == 2) {
                 System.out.println("Игра завершена.");
-                game = false;
+                menu = false;
             } else {
                 System.out.println("Выбрать можно только 1 или 2 вариант. Выбрать можно только 1 или 2 вариант");
             }
