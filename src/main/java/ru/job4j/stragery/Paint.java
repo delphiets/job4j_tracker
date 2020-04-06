9127e0c74205c9cc9040ee6f5201b1c4370cf740
delphiets <delfiyets@gmail.com>
2020-04-06 10:23:49 +0300

    9.1 Рефакторинг теста @Before @After[#245305]

--- src/main/java/ru/job4j/stragery/Paint.java
+++ src/main/java/ru/job4j/stragery/Paint.java
@@ -7,6 +7,7 @@ public class Paint {
 
     public static void main(String[] args) {
         new Paint().draw(new Square());
+        System.out.println();
         new Paint().draw(new Triangle());
     }
 }
