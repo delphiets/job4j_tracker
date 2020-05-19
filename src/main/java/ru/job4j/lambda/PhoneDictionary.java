cac4d86f6587942e3f0e65f252e1c21ad61fefd8
delphiets <delfiyets@gmail.com>
2020-05-19 21:52:45 +0300

    6. В классе StartUI заменить вывод в консоль на Consumer. [#245121]

--- src/main/java/ru/job4j/lambda/PhoneDictionary.java
+++ src/main/java/ru/job4j/lambda/PhoneDictionary.java
@@ -11,7 +11,11 @@ public class PhoneDictionary {
     }
 
     public  ArrayList<Person> find(String key) {
-        Predicate<Person> combine = person ->  person.getName().equals(key) || person.getSurname().equals(key) || person.getPhone().equals(key) || person.getAddress().equals(key);
+        Predicate<Person> combineName = person ->  person.getName().equals(key);
+        Predicate<Person> combineSurname = person -> person.getSurname().equals(key);
+        Predicate<Person> combinePhone = person -> person.getPhone().equals(key);
+        Predicate<Person> combineAddress = person -> person.getAddress().equals(key);
+        Predicate<Person> combine =  combineName.or(combineSurname).or(combinePhone).or(combineAddress);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (combine.test(person)) {
