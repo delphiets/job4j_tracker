1ba2f30ed3d614188dcaeaa6860438082eb3969f
delphiets <delfiyets@gmail.com>
2020-05-24 22:27:50 +0300

    2. Список адресов[110062#245130]

--- src/test/java/ru/job4j/streamapi/ProfileTest.java
+++ src/test/java/ru/job4j/streamapi/ProfileTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.streamapi;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,6 +13,7 @@ public class ProfileTest {
     @Test
     public void consoleAddress() {
         List<Profile> list = List.of(new Profile(new Address("Dnipro", "Grushevsky", 11, 108)), new Profile(new Address("Lviv", "Petrovsky", 1, 10)));
-        assertThat(new Profiles().collect(list).get(0).toString(), is(new Profile(new Address("Dnipro", "Grushevsky", 11, 108)).toString()));
+        List<Address> addresses = new  ArrayList<>(List.of(new Address("Dnipro", "Grushevsky", 11, 108), new Address("Lviv", "Petrovsky", 1, 10)));
+        assertThat(new Profiles().collect(list), is(addresses));
     }
 }
