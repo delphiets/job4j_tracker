3fd604b81a270a6c6af7f29b7c0353b050b5b949
delphiets <delfiyets@gmail.com>
2020-05-30 11:26:27 +0300

    0. Stream API улучшения [#245135]

---
+++ src/main/java/ru/job4j/streamapi/StudentLevel.java
@@ -0,0 +1,17 @@
+package ru.job4j.streamapi;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .filter(st -> st.getScore() > bound)
+                .sorted(Comparator.comparing(Student::getScore))
+                .takeWhile(st -> st.getScore() > bound)
+                .collect(Collectors.toList());
+    }
+}
