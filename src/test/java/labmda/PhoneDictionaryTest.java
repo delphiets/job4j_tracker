715578cc171d29c29b6717b88961292a9c88a95b
delphiets <delfiyets@gmail.com>
2020-05-30 12:13:45 +0300

    2. Local-Variable Type Inference [#245137]

--- src/test/java/labmda/PhoneDictionaryTest.java
+++ src/test/java/labmda/PhoneDictionaryTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import ru.job4j.lambda.Person;
 import ru.job4j.lambda.PhoneDictionary;
 
-import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -13,7 +12,7 @@ public class PhoneDictionaryTest {
     public void whenFindByName() {
         PhoneDictionary phones = new PhoneDictionary();
         phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 }
