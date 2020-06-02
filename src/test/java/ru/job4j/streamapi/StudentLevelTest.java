6d7d0fbbc1f611091ff0adce589ec1c5cdd258b7
delphiets <delfiyets@gmail.com>
2020-06-02 12:37:12 +0300

    0. Stream API улучшения [#245135]

--- src/test/java/ru/job4j/streamapi/StudentLevelTest.java
+++ src/test/java/ru/job4j/streamapi/StudentLevelTest.java
@@ -38,4 +38,18 @@ public class StudentLevelTest {
         List<Student> expected = List.of(new Student("Pety", 28));
         assertThat(StudentLevel.levelOf(input, 10), is(expected));
     }
+    @Test
+    public void whenSecondSorted() {
+        List<Student> input = new ArrayList<>();
+        input.add(new Student("Egor", 19));
+        input.add(new Student("Pety", 128));
+        input.add(new Student("Masha", 28));
+        List<Student> expected = List.of(
+                new Student("Masha", 28),
+                new Student("Pety", 128),
+                new Student("Egor", 19)
+        );
+        System.out.println(input);
+        assertThat(StudentLevel.levelOf(input, 20), is(expected));
+    }
 }
